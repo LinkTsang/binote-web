f44f0d67d5d16579e63846b2b8a0756a092435ee
Link <linkdoyle@163.com>
2021-03-27 22:56:24 +0800

    [feat] Add editor demo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ZEditor from './editor/ZEditor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">ZNote Demo</header>
+      <div style={{ height: '100%' }}>
+        <ZEditor />
+      </div>
     </div>
   );
 }
---
+++ src/editor/ZEditor.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Editor, EditorState } from 'draft-js';
+import 'draft-js/dist/Draft.css';
+
+function ZEditor() {
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty()
+  );
+
+  return (
+    <Editor
+      editorState={editorState}
+      onChange={setEditorState}
+      placeholder="Write something!"
+    />
+  );
+}
+
+export default ZEditor;
