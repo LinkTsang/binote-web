94f84a36291d869d1f47b50b9f933747d22dbd35
Link <linkdoyle@163.com>
2021-07-10 10:51:43 +0800

    [func] Add local storage persistence.

--- src/editor/ZEditor.tsx
+++ src/editor/ZEditor.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Editor, EditorState } from 'draft-js';
+import {
+  Editor,
+  EditorState,
+  convertToRaw,
+  ContentState,
+  convertFromRaw,
+} from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
+const loadContentFromLocalStorage = () => {
+  const content = window.localStorage.getItem('content');
+  if (content) {
+    return EditorState.createWithContent(convertFromRaw(JSON.parse(content)));
+  } else {
+    return EditorState.createEmpty();
+  }
+};
+
+const saveContentToLocalStorage = (content: ContentState) => {
+  window.localStorage.setItem('content', JSON.stringify(convertToRaw(content)));
+};
+
 function ZEditor() {
-  const [editorState, setEditorState] = React.useState(() =>
-    EditorState.createEmpty()
+  const [editorState, setEditorState] = React.useState(
+    loadContentFromLocalStorage
   );
+
+  const onChange = (state: EditorState) => {
+    const currentContent = state.getCurrentContent();
+    saveContentToLocalStorage(currentContent);
+    setEditorState(state);
+  };
 
   return (
     <Editor
       editorState={editorState}
-      onChange={setEditorState}
+      onChange={onChange}
       placeholder="Write something!"
     />
   );
