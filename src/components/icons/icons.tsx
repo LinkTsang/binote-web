19f45f68fa22f518684e8af33a2fc38a03b0b68c
Link <linkdoyle@163.com>
2021-07-31 00:30:16 +0800

    [func] Export icons.

---
+++ src/components/icons/icons.tsx
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 Bin Tsang
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export { default as HeadNIcon } from './HeadNIcon';
+export { default as Head1Icon } from './Head1Icon';
+export { default as Head2Icon } from './Head2Icon';
+export { default as Head3Icon } from './Head3Icon';
+export { default as Head4Icon } from './Head4Icon';
+export { default as Head5Icon } from './Head5Icon';
+export { default as Head6Icon } from './Head6Icon';
+export { default as Head7Icon } from './Head7Icon';
+export { default as Head8Icon } from './Head8Icon';
+export { default as Head9Icon } from './Head9Icon';
+export { default as QuoteIcon } from './QuoteIcon';
+export { default as CodeIcon } from './CodeIcon';
+export { default as CodeBlockIcon } from './CodeBlockIcon';
---
+++ src/components/icons/index.tsx
@@ -0,0 +1,6 @@
+// Copyright (c) 2021 Bin Tsang
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export * from './icons';
