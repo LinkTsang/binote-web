93e806169e2dc8bedfb3822709c24b4f6f35efa9
Link <linkdoyle@163.com>
2021-07-31 02:00:02 +0800

    [func] Move toolbar into `ZEditor` and show toolbar when selected.

--- src/components/editor/ZEditor.tsx
+++ src/components/editor/ZEditor.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, MouseEvent, useRef } from 'react';
 import {
   Editor,
   EditorState,
@@ -7,7 +7,69 @@ import {
   convertFromRaw,
 } from 'draft-js';
 import 'draft-js/dist/Draft.css';
-import { Input } from 'antd';
+import { Button, Input, Popover } from 'antd';
+import {
+  BoldOutlined,
+  ItalicOutlined,
+  HighlightOutlined,
+  UnderlineOutlined,
+  StrikethroughOutlined,
+  OrderedListOutlined,
+  UnorderedListOutlined,
+  CodeOutlined,
+  EllipsisOutlined,
+} from '@ant-design/icons';
+import {
+  HeadNIcon,
+  Head1Icon,
+  Head2Icon,
+  Head3Icon,
+  Head4Icon,
+  Head5Icon,
+  Head6Icon,
+  Head7Icon,
+  Head8Icon,
+  Head9Icon,
+  QuoteIcon,
+  CodeIcon,
+  CodeBlockIcon,
+} from '../../components/icons';
+
+const ToolHeader = (
+  <div>
+    <Button icon={<Head1Icon />}></Button>
+    <Button icon={<Head2Icon />}></Button>
+    <Button icon={<Head3Icon />}></Button>
+    <Button icon={<Head4Icon />}></Button>
+    <Button icon={<Head5Icon />}></Button>
+    <Button icon={<Head6Icon />}></Button>
+    <Button icon={<Head7Icon />}></Button>
+    <Button icon={<Head8Icon />}></Button>
+    <Button icon={<Head9Icon />}></Button>
+  </div>
+);
+
+const ToolBar = () => {
+  return (
+    <div>
+      <Popover content={ToolHeader}>
+        <Button icon={<HeadNIcon />}></Button>
+      </Popover>
+      <Button icon={<BoldOutlined />}></Button>
+      <Button icon={<ItalicOutlined />}></Button>
+      <Button icon={<HighlightOutlined />}></Button>
+      <Button icon={<UnderlineOutlined />}></Button>
+      <Button icon={<StrikethroughOutlined />}></Button>
+      <Button icon={<OrderedListOutlined />}></Button>
+      <Button icon={<UnorderedListOutlined />}></Button>
+      <Button icon={<CodeOutlined />}></Button>
+      <Button icon={<EllipsisOutlined />}></Button>
+      <Button icon={<QuoteIcon />}></Button>
+      <Button icon={<CodeIcon />}></Button>
+      <Button icon={<CodeBlockIcon />}></Button>
+    </div>
+  );
+};
 
 const loadContentFromLocalStorage = () => {
   const content = window.localStorage.getItem('content');
@@ -23,8 +85,18 @@ const saveContentToLocalStorage = (content: ContentState) => {
 };
 
 function ZEditor(props: { onTitleChange?: (title: string) => void }) {
+  const elementRef = useRef<HTMLDivElement>(null);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [toolbarVisible, setToolbarVisible] = useState(false);
   const [title, setTitle] = useState('Binote Demo');
   const [editorState, setEditorState] = useState(loadContentFromLocalStorage);
+
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    if (elementRef.current) {
+      const rect = elementRef.current.getBoundingClientRect();
+      setMousePosition({ x: e.clientX - rect.x, y: e.clientY - rect.y });
+    }
+  };
 
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const newTitle = e.target.value;
@@ -32,14 +104,34 @@ function ZEditor(props: { onTitleChange?: (title: string) => void }) {
     props.onTitleChange?.(newTitle);
   };
 
-  const handleContentChange = (state: EditorState) => {
+  const handleEditorChange = (state: EditorState) => {
+    const selectionState = editorState.getSelection();
+    if (!selectionState.isCollapsed()) {
+      setToolbarVisible(true);
+    }
+
     const currentContent = state.getCurrentContent();
     saveContentToLocalStorage(currentContent);
     setEditorState(state);
   };
 
   return (
-    <div>
+    <div ref={elementRef} onMouseMove={handleMouseMove}>
+      <div
+        style={{
+          position: 'absolute',
+          left: `${mousePosition.x}px`,
+          top: `${mousePosition.y}px`,
+        }}
+      >
+        <Popover
+          content={<ToolBar />}
+          trigger="click"
+          visible={toolbarVisible}
+          onVisibleChange={setToolbarVisible}
+          arrowPointAtCenter={false}
+        ></Popover>
+      </div>
       <p>
         <Input
           placeholder="Please enter title"
@@ -51,7 +143,7 @@ function ZEditor(props: { onTitleChange?: (title: string) => void }) {
       </p>
       <Editor
         editorState={editorState}
-        onChange={handleContentChange}
+        onChange={handleEditorChange}
         placeholder="Write something!"
       />
     </div>
--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -4,49 +4,9 @@
 // https://opensource.org/licenses/MIT
 
 import { useState } from 'react';
-import { Button, Card, Layout, Popover } from 'antd';
+import { Card, Layout } from 'antd';
 import { Content, Header } from 'antd/lib/layout/layout';
-import {
-  BoldOutlined,
-  ItalicOutlined,
-  HighlightOutlined,
-  UnderlineOutlined,
-  StrikethroughOutlined,
-  OrderedListOutlined,
-  UnorderedListOutlined,
-  CodeOutlined,
-  EllipsisOutlined,
-} from '@ant-design/icons';
-import {
-  HeadNIcon,
-  Head1Icon,
-  Head2Icon,
-  Head3Icon,
-  Head4Icon,
-  Head5Icon,
-  Head6Icon,
-  Head7Icon,
-  Head8Icon,
-  Head9Icon,
-  QuoteIcon,
-  CodeIcon,
-  CodeBlockIcon,
-} from '../components/icons';
 import ZEditor from '../components/editor/ZEditor';
-
-const ToolHeader = (
-  <div>
-    <Button icon={<Head1Icon />}></Button>
-    <Button icon={<Head2Icon />}></Button>
-    <Button icon={<Head3Icon />}></Button>
-    <Button icon={<Head4Icon />}></Button>
-    <Button icon={<Head5Icon />}></Button>
-    <Button icon={<Head6Icon />}></Button>
-    <Button icon={<Head7Icon />}></Button>
-    <Button icon={<Head8Icon />}></Button>
-    <Button icon={<Head9Icon />}></Button>
-  </div>
-);
 
 function EditPage() {
   const [title, setTitle] = useState('Binote Demo');
@@ -55,23 +15,6 @@ function EditPage() {
       <Header>
         <p style={{ color: 'white' }}>{title ? title : 'untitled article'}</p>
       </Header>
-      <div>
-        <Popover content={ToolHeader}>
-          <Button icon={<HeadNIcon />}></Button>
-        </Popover>
-        <Button icon={<BoldOutlined />}></Button>
-        <Button icon={<ItalicOutlined />}></Button>
-        <Button icon={<HighlightOutlined />}></Button>
-        <Button icon={<UnderlineOutlined />}></Button>
-        <Button icon={<StrikethroughOutlined />}></Button>
-        <Button icon={<OrderedListOutlined />}></Button>
-        <Button icon={<UnorderedListOutlined />}></Button>
-        <Button icon={<CodeOutlined />}></Button>
-        <Button icon={<EllipsisOutlined />}></Button>
-        <Button icon={<QuoteIcon />}></Button>
-        <Button icon={<CodeIcon />}></Button>
-        <Button icon={<CodeBlockIcon />}></Button>
-      </div>
       <Content>
         <Card>
           <ZEditor onTitleChange={setTitle} />
