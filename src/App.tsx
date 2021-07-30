b227ec283a55e6f621f392fa39f5ed8abea5acb0
Link <linkdoyle@163.com>
2021-07-30 09:14:53 +0800

    [func] Move router to `App`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
+import EditPage from './pages/EditPage';
+import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <HomePage />
+        </Route>
+        <Route path="/register">
+          <RegisterPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+        <Route path="/edit">
+          <EditPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
