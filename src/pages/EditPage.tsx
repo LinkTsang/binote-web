596d0f2355ab629beb80538818a146d38d7f9712
Link <linkdoyle@163.com>
2021-07-30 22:39:31 +0800

    [func] Add toolbar ui.

--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -3,10 +3,35 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import { Card, Layout } from 'antd';
-import { Content, Header } from 'antd/lib/layout/layout';
 import { useState } from 'react';
+import { Button, Card, Layout, Popover } from 'antd';
+import { Content, Header } from 'antd/lib/layout/layout';
+import {
+  BoldOutlined,
+  ItalicOutlined,
+  HighlightOutlined,
+  UnderlineOutlined,
+  StrikethroughOutlined,
+  OrderedListOutlined,
+  UnorderedListOutlined,
+  CodeOutlined,
+  EllipsisOutlined,
+} from '@ant-design/icons';
 import ZEditor from '../components/editor/ZEditor';
+import QuoteIcon from '../components/icons/QuoteIcon';
+import CodeIcon from '../components/icons/CodeIcon';
+import CodeBlockIcon from '../components/icons/CodeBlockIcon';
+
+const ToolHeader = (
+  <div>
+    <Button>H1</Button>
+    <Button>H2</Button>
+    <Button>H3</Button>
+    <Button>H4</Button>
+    <Button>H5</Button>
+    <Button>H6</Button>
+  </div>
+);
 
 function EditPage() {
   const [title, setTitle] = useState('Binote Demo');
@@ -15,6 +40,23 @@ function EditPage() {
       <Header>
         <p style={{ color: 'white' }}>{title ? title : 'untitled article'}</p>
       </Header>
+      <div>
+        <Popover content={ToolHeader}>
+          <Button>H</Button>
+        </Popover>
+        <Button icon={<BoldOutlined />}></Button>
+        <Button icon={<ItalicOutlined />}></Button>
+        <Button icon={<HighlightOutlined />}></Button>
+        <Button icon={<UnderlineOutlined />}></Button>
+        <Button icon={<StrikethroughOutlined />}></Button>
+        <Button icon={<OrderedListOutlined />}></Button>
+        <Button icon={<UnorderedListOutlined />}></Button>
+        <Button icon={<CodeOutlined />}></Button>
+        <Button icon={<EllipsisOutlined />}></Button>
+        <Button icon={<QuoteIcon />}></Button>
+        <Button icon={<CodeIcon />}></Button>
+        <Button icon={<CodeBlockIcon />}></Button>
+      </div>
       <Content>
         <Card>
           <ZEditor onTitleChange={setTitle} />
