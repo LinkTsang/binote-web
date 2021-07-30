ce9d6b0dbfec8cf7c8619a1438c6a94171b205ae
Link <linkdoyle@163.com>
2021-07-30 10:10:41 +0800

    [func] Update `RegisterPage`.

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -3,12 +3,58 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-function RegisterPage() {
+import { Button, Checkbox, Form, Input } from 'antd';
+import { UserOutlined, MailOutlined, LockOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+const RegisterPage = () => {
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
-    <div>
-      <p>RegisterPage</p>
-    </div>
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: 'Please input your Username!' }]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Username"
+        />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        rules={[{ required: true, message: 'Please input your Email!' }]}
+      >
+        <Input
+          prefix={<MailOutlined className="site-form-item-icon" />}
+          placeholder="Email"
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Sign Up
+        </Button>
+        Or <Link to="/login">Login with an existing account</Link>
+      </Form.Item>
+    </Form>
   );
-}
+};
 
 export default RegisterPage;
