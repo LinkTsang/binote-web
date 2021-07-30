1214f71381e6ca7eb15fd597162c32f8e70bb0ef
Link <linkdoyle@163.com>
2021-07-30 11:30:02 +0800

    [func] Update `HomePage`.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,18 +3,21 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import { Button, Space } from 'antd';
 import { Link } from 'react-router-dom';
+import styles from './HomePage.module.css';
 
 function HomePage() {
   return (
-    <div>
-      <p>HomePage</p>
-      <p>
-        <Link to="/login">Login</Link>
-      </p>
-      <p>
-        <Link to="/article">Article</Link>
-      </p>
+    <div className={styles.HomePage}>
+      <h1>Z-Note</h1>
+      <p>Yet another note demo.</p>
+      <Space>
+        <Button type="primary" href="/register">
+          Start
+        </Button>
+        <Button href="/article">Explore</Button>
+      </Space>
     </div>
   );
 }
