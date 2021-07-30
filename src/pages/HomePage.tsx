964508555c08bd9f60f835d86a4e05a596952b9f
Link <linkdoyle@163.com>
2021-07-30 09:52:43 +0800

    [func] Add link `Login`.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,10 +3,13 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import { Link } from "react-router-dom";
+
 function HomePage() {
   return (
     <div>
       <p>HomePage</p>
+      <Link to="/login">Login</Link>
     </div>
   );
 }
