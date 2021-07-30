8a815f8d75e4fe528255412bc0d21457a13a6967
Link <linkdoyle@163.com>
2021-07-30 10:15:44 +0800

    [func] Add link `/edit`.

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -3,10 +3,15 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import { Link } from 'react-router-dom';
+
 function ArticlePage() {
   return (
     <div>
       <p>ArticlePage</p>
+      <p>
+        <Link to="/edit">Edit</Link>
+      </p>
     </div>
   );
 }
